47faa6fce7e449ba0e862d77bc0996d3c2f17bd1
Riley Jameson <rjameson96@gmail.com>
2025-02-23 18:50:08 -0500

    Added cool leafbillowing background

---
+++ quartz/components/LeafBillowing.tsx
@@ -0,0 +1,38 @@
+import { QuartzComponentConstructor } from "./types"
+import style from "./styles/leafBillowing.scss"
+
+function LeafBillowing() {
+  return (
+    <div className="leaf-container">
+      <div id="leaves">
+        <svg style={{ width: 0, height: 0, position: "absolute" }}>
+          <defs>
+            <filter id="wind" x="-20%" y="-20%" width="140%" height="140%">
+              <feTurbulence type="fractalNoise" numOctaves="2" seed="1">
+                <animate
+                  attributeName="baseFrequency"
+                  dur="16s"
+                  keyTimes="0;0.33;0.66;1"
+                  values="0.005 0.003;0.01 0.009;0.008 0.004;0.005 0.003"
+                  repeatCount="indefinite"
+                />
+              </feTurbulence>
+              <feDisplacementMap in="SourceGraphic">
+                <animate
+                  attributeName="scale"
+                  dur="20s"
+                  keyTimes="0;0.25;0.5;0.75;1"
+                  values="45;55;75;55;45"
+                  repeatCount="indefinite"
+                />
+              </feDisplacementMap>
+            </filter>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+LeafBillowing.css = style
+export default (() => LeafBillowing) satisfies QuartzComponentConstructor
