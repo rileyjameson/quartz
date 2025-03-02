5961a9ebd3056b2d7fc8b8a26f1e5e4b511a850d
Riley Jameson <rjameson96@gmail.com>
2025-03-02 18:29:26 -0500

    Trying again to hid palm tree on mobile

--- quartz/components/LeafBillowing.tsx
+++ quartz/components/LeafBillowing.tsx
@@ -2,6 +2,11 @@ import { QuartzComponentConstructor } from "./types"
 import style from "./styles/leafBillowing.scss"
 
 function LeafBillowing() {
+  // Only render on desktop
+  if (typeof window !== "undefined" && window.innerWidth <= 768) {
+    return null
+  }
+
   return (
     <div className="leaf-container">
       <div id="leaves">
