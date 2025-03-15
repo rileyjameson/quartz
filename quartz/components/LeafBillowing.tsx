d53028632ce421e082313f3e11d6e1beeb6c1cb3
Riley Jameson <rjameson96@gmail.com>
2025-03-15 14:40:47 -0400

    removed palm tree swaying, made palm tree smaller, changed light and dark mode colors to be more subtle

--- quartz/components/LeafBillowing.tsx
+++ quartz/components/LeafBillowing.tsx
@@ -9,42 +9,7 @@ function LeafBillowing() {
 
   return (
     <div className="leaf-container">
-      <div id="leaves">
-        <svg style={{ width: 0, height: 0, position: "absolute" }}>
-          <defs>
-            <filter id="wind" x="-20%" y="-20%" width="140%" height="140%">
-              {/* Reduced turbulence frequency and octaves */}
-              <feTurbulence type="fractalNoise" baseFrequency="0.005 0.002" numOctaves="1" seed="3">
-                {/* Slower, more subtle frequency changes */}
-                <animate
-                  attributeName="baseFrequency"
-                  dur="18s"
-                  keyTimes="0;0.25;0.5;0.75;1"
-                  values="0.005 0.002;0.004 0.0015;0.005 0.002;0.006 0.0025;0.005 0.002"
-                  repeatCount="indefinite"
-                />
-              </feTurbulence>
-
-              {/* Gentler displacement that works better with CSS animations */}
-              <feDisplacementMap
-                in="SourceGraphic"
-                scale="20"
-                xChannelSelector="R"
-                yChannelSelector="G"
-              >
-                {/* Subtle scale variation that won't cause rippling */}
-                <animate
-                  attributeName="scale"
-                  dur="15s"
-                  keyTimes="0;0.33;0.66;1"
-                  values="20;25;15;20"
-                  repeatCount="indefinite"
-                />
-              </feDisplacementMap>
-            </filter>
-          </defs>
-        </svg>
-      </div>
+      <div id="leaves">{/* Removed SVG filter */}</div>
     </div>
   )
 }
